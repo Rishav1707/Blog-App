2d98229d4fd998bbcc1d6d2199ba9b02d13cb46f
Rishav Raj <rishavraj1707music@gmail.com>
2023-06-09 10:43:34 +0530

    [feat] Add User Details in Blog Items

--- Make-Blog/src/components/BlogItem.jsx
+++ Make-Blog/src/components/BlogItem.jsx
@@ -5,6 +5,12 @@ export default function BlogItem({ blog }) {
       {blog.image && <img className="Blog-image" src={blog.image} />}
       <h2 className="Blog-Title">{blog.title}</h2>
       <p className="Blog-Content">{blog.content}</p>
+      <div className="userDetails">
+        <h3 className="userName">
+          <img className="userImage" src={blog.userImage} />
+          {blog.userName}
+        </h3>
+      </div>
     </div>
   );
 }
